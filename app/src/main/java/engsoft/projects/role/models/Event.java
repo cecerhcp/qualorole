6e4b9da3407c160be3c3ebd5af6d0791bf15a1f4
cecerhcp <cecerhcp0@hotmail.com>
2016-12-04 13:47:06 -0200

    esqueleto do projeto tá pronto, só falta codar

---
+++ app/src/main/java/engsoft/projects/role/models/Event.java
@@ -0,0 +1,7 @@
+package engsoft.projects.role.models;
+
+/**
+ * Created by tevredengebruiker on 12/4/2016.
+ */
+public class Event {
+}
---
+++ app/src/main/java/engsoft/projects/role/presenters/SearchEventPresenter.java
@@ -0,0 +1,7 @@
+package engsoft.projects.role.presenters;
+
+/**
+ * Created by tevredengebruiker on 12/4/2016.
+ */
+public class SearchEventPresenter {
+}
